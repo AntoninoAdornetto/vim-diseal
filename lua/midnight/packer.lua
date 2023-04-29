c042f80eaeea13a72f2a37ee4397502bf3054276
Antonino Adornetto <armaninobilling@gmail.com>
2023-04-29 05:38:35 -0400

    [EDIT] vsbunk theme

--- lua/midnight/packer.lua
+++ lua/midnight/packer.lua
@@ -6,6 +6,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
+	use 'Mofiqul/vscode.nvim'
 
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.1',
@@ -13,8 +14,6 @@ return require('packer').startup(function(use)
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
 
-	use({ 'rose-pine/neovim', as = 'rose-pine' })
-	vim.cmd('colorscheme rose-pine')
 
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 	use('theprimeagen/harpoon')
