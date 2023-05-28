b2b725a1f4e84b86d4094e81b90b1cf870c62f23
Antonino Adornetto <armaninobilling@gmail.com>
2023-05-28 14:32:01 -0400

    [EDIT] clang formatter for C files

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -2,10 +2,11 @@ local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-	filetypes = {"go"},
+	filetypes = {"go", "c"},
 	sources = {
 		null_ls.builtins.formatting.gofmt,
 		null_ls.builtins.formatting.golines,
+		null_ls.builtins.formatting.clang_format,
 		-- goimports reviser will be nice to use on a new project. revist
 		-- null_ls.builtins.formatting.goimports_reviser,
 	},
