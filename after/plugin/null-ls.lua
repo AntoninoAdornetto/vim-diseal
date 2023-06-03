f72b34c370f5f2e36a2c2265f4d3676e6fad0937
Antonino Adornetto <armaninobilling@gmail.com>
2023-06-03 02:16:24 -0400

    [EDIT] rms unused imports

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -6,6 +6,7 @@ null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.gofmt,
 		null_ls.builtins.formatting.golines,
+		null_ls.builtins.formatting.goimports,
 		null_ls.builtins.formatting.clang_format,
 		-- goimports reviser will be nice to use on a new project. revist
 		-- null_ls.builtins.formatting.goimports_reviser,
