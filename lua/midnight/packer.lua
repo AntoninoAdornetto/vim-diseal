516bf352211db2478dac07947044f8d66a09414b
Antonino Adornetto <armaninobilling@gmail.com>
2023-08-18 00:55:03 -0400

    [EDIT] new theme when bored

--- lua/midnight/packer.lua
+++ lua/midnight/packer.lua
@@ -12,6 +12,7 @@ return require('packer').startup(function(use)
 	use 'Mofiqul/dracula.nvim'
 	use 'ellisonleao/gruvbox.nvim'
 	use 'shaunsingh/nord.nvim'
+	use { "catppuccin/nvim", as = "catppuccin" }
 
 	-- debuggers
 	use 'mfussenegger/nvim-dap'
