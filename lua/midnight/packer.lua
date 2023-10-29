e8a04c52de919f04b60310e980d69b55e8acb4c0
Antonino Adornetto <armaninobilling@gmail.com>
2023-10-29 16:47:57 -0400

    feat: markdown preview plugin

--- lua/midnight/packer.lua
+++ lua/midnight/packer.lua
@@ -89,4 +89,13 @@ return require('packer').startup(function(use)
 		{'MunifTanjim/prettier.nvim'},
 	}
 }
+
+
+	-- markdown preview
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function() vim.fn["mkdp#util#install"]() end,
+	})
+
+	use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })	
 end)
